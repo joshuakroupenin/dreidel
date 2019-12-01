1d399a91af55e18f4e92d92962871c9d486a8159
Joshua Kroupenin <joshua.kroupenin@gmail.com>
2019-12-01 14:26:05 -0500

    Hebrew letters added

--- DreidelSpin/index.js
+++ DreidelSpin/index.js
@@ -1,12 +1,13 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    var dreidel = ["Gimmel", "Hay", "Shin","Nun"];
+    var dreidel = ["Nun", "Gimmel", "Hay", "Shin"];
+    var hebrew = ["נ","ג","ה","ש"];
    
-    letter = Math.floor(Math.random() * 4);     // returns a random integer from 0 to 3
+    letter_index = Math.floor(Math.random() * 4);     // returns a random integer from 0 to 3
    
     context.res = {
             // status: 200, /* Defaults to 200 */
-            body: dreidel[letter]
+            body: dreidel[letter_index] + ":" + hebrew[letter_index]
     }
 };
